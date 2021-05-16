49fe4c7d8560cd1c871337b786212bb6d865b501
Carlo D. Romero <carlo.romero1991@outlook.com>
2021-05-16 00:21:12 -0700

    unused code cleanup

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,21 +1,10 @@
 import React from "react";
-// import React, { useState } from "react";
+
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-  /*
-  const [title, setTitle] = useState(props.title);
-  console.log("ExpenseItem is evaluated by React");
-  const clickHandler = () => {
-    setTitle("Updated!");
-    console.log(title);
-  };
-  <h2>{title}</h2>
-  <button onClick={clickHandler}>Change Title</button>
-  */
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
