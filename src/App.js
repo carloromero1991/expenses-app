230941f891dfe6ecb28d1b0b9afd8ea04dc8f77e
carlo.romero1991 <carlo.romero007@gmail.com>
2021-05-17 15:19:06 -0700

    added conditional items rendering;

--- src/App.js
+++ src/App.js
@@ -3,34 +3,34 @@ import React, { useState } from "react";
 import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
 
-const DUMMY_EXPENSES = [
-  {
-    id: "e1",
-    title: "Toilet Paper",
-    amount: 94.12,
-    date: new Date(2020, 7, 14),
-  },
-  {
-    id: "e2",
-    title: "New TV",
-    amount: 799.49,
-    date: new Date(2021, 2, 12),
-  },
-  {
-    id: "e3",
-    title: "Car Insurance",
-    amount: 294.67,
-    date: new Date(2020, 2, 28),
-  },
-  {
-    id: "e4",
-    title: "New Desk (Wooden)",
-    amount: 450,
-    date: new Date(2021, 5, 12),
-  }
-];
-
 function App() {
+  const DUMMY_EXPENSES = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    {
+      id: "e2",
+      title: "New TV",
+      amount: 799.49,
+      date: new Date(2021, 2, 12),
+    },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2020, 2, 28),
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+
   const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = (expense) => {
@@ -39,10 +39,18 @@ function App() {
     });
   };
 
+  let content = (
+    <p style={{ textAlign: "center" }}>No expenses saved. Maybe add one?</p>
+  );
+
+  if (expenses.length > 0) {
+    content = <Expenses items={expenses} />;
+  }
+
   return (
     <div className="center-div">
       <NewExpense onAddExpense={addExpenseHandler} />
-      <Expenses items={expenses} />
+      {content}
     </div>
   );
 }
