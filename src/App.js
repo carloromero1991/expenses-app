107afe702860e2a8372695bf05420b73c9d5b065
carlo.romero1991 <carlo.romero007@gmail.com>
2021-05-18 16:07:26 -0700

    -erased commented code; *validated form; *style

--- src/App.js
+++ src/App.js
@@ -9,33 +9,6 @@ import IndexedDB from "./IndexedDB";
 let gotData = false;
 
 function App() {
-  /*   const DUMMY_EXPENSES = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    {
-      id: "e2",
-      title: "New TV",
-      amount: 799.49,
-      date: new Date(2021, 2, 12),
-    },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2020, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ]; */
-
   const [expenses, setExpenses] = useState({});
   const idbConfig = {
     dbName: "expenses-app",
@@ -56,10 +29,9 @@ function App() {
   }
 
   const addExpenseHandler = (expense) => {
-    console.log("addExpenseHandler", expense);
-
+    // console.log("addExpenseHandler", expense);
     IndexedDB.SaveDataAsync(idbConfig, expense)
-      .then((data) => {
+      .then((result) => {
         gotData = true;
         // console.log("IndexedDB().SaveDataAsync() from App has finished");
 
